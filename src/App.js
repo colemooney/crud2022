d0aede130f6c7af859970ea4ee21f0147b751f76
Cole <colemooney5@gmail.com>
2022-01-28 19:46:01 -0800

    new form page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import Radio from "@material-ui/core/Radio";
-import Select from "@material-ui/core/Select";
+import Box from "@material-ui/core/Box";
 import MenuItem from "@material-ui/core/MenuItem";
 import Slider from "@material-ui/core/Slider";
-import Button from "@material-ui/core/Button";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import Checkbox from '@material-ui/core/Checkbox';
+
+
 import './App.css';
 
 const defaultValues = {
   name: "",
-  age: 0,
-  gender: "",
-  os: "",
-  favoriteNumber: 0,
+  party: 0,
+  occasion: "",
+  
 };
 
 
@@ -38,71 +41,45 @@ function App() {
     console.log(formValues);
   };
 
-
+  const paperStyle={padding :20,height:'60vh',width:280, margin:"20px auto"}
+  const avatarStyle={backgroundColor:'#1bbd7e'}
+  const btnstyle={margin:'8px 0'}
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid container alignItems="center" justify="center" direction="column">
-        <Grid item>
-          <TextField
-            id="name-input"
-            name="name"
-            label="Reservation Name"
-            type="text"
-            value={formValues.name}
-            onChange={handleInputChange}
-          />
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+    <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                     
+                    <h2>Reservation CRUD</h2>
+                </Grid>
+                <TextField label='Party Name' placeholder="Enter Party Name" fullWidth required/>
+                <TextField label='Party Number' placeholder='How many in the party?' type='password' fullWidth required/>
+                <FormControlLabel
+                    control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember me"
+                 />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
         </Grid>
-        <Grid item>
-          <TextField
-            id="age-input"
-            name="party"
-            label="Party Number"
-            type="number"
-            value={formValues.age}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item>
-          <FormControl>
-            <FormLabel>Special Occasion</FormLabel>
-            <RadioGroup
-              name="occasion"
-              value={formValues.gender}
-              onChange={handleInputChange}
-              row
-            >
-              <FormControlLabel
-                key="birthday"
-                value="birthday"
-                control={<Radio size="small" />}
-                label="Birthday"
-              />
-              <FormControlLabel
-                key="anniversary"
-                value="anniversary"
-                control={<Radio size="small" />}
-                label="Anniversary"
-              />
-              <FormControlLabel
-                key="other"
-                value="other"
-                control={<Radio size="small" />}
-                label="Other"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item>
-          
-        </Grid>
-        <Grid item>
-          
-        </Grid>
-        <Button variant="contained" color="primary" type="submit">
-          Submit
-        </Button>
-      </Grid>
-    </form>
+        </ form>
+    </div>
+
   );
 };
 
